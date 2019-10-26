ca120a3c1ac0977e970eb0f6d74f76f872a4b3c6
kishorkumardas <kishorworld16@gmail.com>
2019-10-26 15:10:01 +0600

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,27 +1,3 @@
-// var endPoint="https://api.whatdoestrumpthink.com/api/v1/quotes/random";
-
-// function getQuote(){
-//     fetch(endPoint)
-//         .then(function(response){
-//             return response.json();
-//         })
-//         .then(function(data){
-//             displayQuote(data.message);
-//         })
-//         .catch(function(){
-//             console.log('an error occurred');
-//         })
-// }
-// function displayQuote(quote){
-//     var quoteText=document.querySelector('.quote-text');
-//     quoteText.textContent=quote;
-// }
-// var newQuoteButton= document.querySelector('.new-quote');
-
-// newQuoteButton.addEventListener('click',getQuote);
-
-// getQuote();
-
 var endPoint="https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
 var newQuoteButton=document.querySelector('.new-quote');
